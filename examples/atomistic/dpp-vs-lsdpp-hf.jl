9ebc140a8ada339a13b810980c549695c38ac791
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2025-09-19 14:22:04 -0400

    Update atomistic example

--- examples/atomistic/dpp-vs-lsdpp-hf.jl
+++ examples/atomistic/dpp-vs-lsdpp-hf.jl
@@ -39,14 +39,14 @@ file_paths = ["$path/Hf128_MC_rattled_mp100_form_sorted.extxyz",
 
 
 # Sample size and dataset size
-n = 1 # 200
+n = 200
 ch = chunk_iterator(file_paths; chunksize=1000)
-N = ch.data[ch.n_avail_items][2][end] # 4378
+N = maximum([ maximum(ch.data[i][2]) for i in 1:ch.n_avail_items]) # 4378
 
 # Sampling by DPP
 Random.seed!(42) # Fix seed to compare DPP and LSDPP: get same random chunks
 @time begin
-    ch = chunk_iterator(file_paths; chunksize=N)
+    ch = chunk_iterator(file_paths; chunksize=N, randomized=false)
     chunk, _ = take!(ch)
     features = create_features(chunk)
     D = pairwise(Euclidean(), features')
@@ -63,7 +63,7 @@ GC.gc()
 # Sampling by LSDPP
 Random.seed!(42) # Fixed seed to compare DPP and LSDPP: get same random chunks
 @time begin
-    lsdpp = LSDPP(file_paths; chunksize=1000, subchunksize=100, max=N)
+    lsdpp = LSDPP(file_paths; chunksize=2000, subchunksize=200, max=N)
     lsdpp_probs = inclusion_prob(lsdpp, n)
     lsdpp_indexes = sample(lsdpp, n)
 end
@@ -81,7 +81,7 @@ savefig("dpp-probs-vs-lsdpp-probs-hf.png")
 
 # DPP theoretical inclusion probabilities vs LSDPP inclusion frequencies when
 # sampling n points from a set of size N, with each point of size M
-iterations = 20_000_00 # Use 20_000_000
+iterations = 100_000 # Use 20_000_000
 lsdpp_freqs = relative_frequencies(lsdpp, n, iterations)
 scatter(dpp_probs, lsdpp_freqs, color="red", alpha=0.5)
 plot!(dpp_probs, dpp_probs, color="blue", alpha=0.5)
@@ -93,7 +93,7 @@ savefig("dpp-probs-vs-lsdpp-freqs-hf.png")
 # DPP theoretical inclusion probabilities vs LSDPP inclusion frequencies of 2 
 # random points, when sampling n points from a set of size N, with each point of size M
 set = rand(1:N, 2)
-iterations = 10 # Use 10_000_000
+iterations = 100_000 # Use 10_000_000
 lsdpp_set_freqs = relative_frequencies(lsdpp, set, n, iterations)
 dpp_set_freqs = det(marginal_kernel(dpp)[set, set])
 @printf("DPP inclusion probability for dataset %s is %f \n", 
