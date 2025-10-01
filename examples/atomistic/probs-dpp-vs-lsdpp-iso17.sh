8127d54af46b10648ad5e0bc9dab9cdf0118db28
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2025-10-01 14:49:08 -0400

    scripts for engaging and supercloud

---
+++ examples/atomistic/probs-dpp-vs-lsdpp-iso17.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Loading the required module
+# source /etc/profile
+# Job Flags
+#SBATCH -p mit_preemptable
+#SBATCH --mem=1024G
+#module load mpi/openmpi-5.0.5 # Supercloud
+#module load julia/1.11.3 # Supercloud
+module load openmpi/5.0.6 # Engaging
+module load julia/1.10.4 # Engaging
+# Install Julia packages
+julia --project=./ -e 'import Pkg; Pkg.instantiate()'
+# Run the script
+julia --project=./ probs-dpp-vs-lsdpp-iso17.jl
