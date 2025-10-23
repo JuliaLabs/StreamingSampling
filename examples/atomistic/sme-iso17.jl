c412dddfcbefa9aef76eb36b639de86fad221df2
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2025-10-23 13:14:26 -0400

    Organazing flies

--- examples/atomistic/sme-iso17.jl
+++ examples/atomistic/sme-iso17.jl
@@ -7,11 +7,11 @@ include("utils/atom-conf-features-extxyz.jl")
 # Basis function to compute ACE descriptors (features)
 basis = ACE(species           = [:C, :O, :H],
             body_order        = 4,
-            polynomial_degree = 4,
-            rcutoff           = 5.0,
-            wL                = 1.0,
+            polynomial_degree = 12,
+            wL                = 2.0,
             csp               = 1.0,
-            r0                = 1.0);
+            r0                = 1.43,
+            rcutoff           = 4.4 );
 
 # Dataset
 file_paths = ["data/iso17/my_iso17_train.extxyz"] 
@@ -20,16 +20,3 @@ file_paths = ["data/iso17/my_iso17_train.extxyz"]
 sme = StreamMaxEnt(file_paths; chunksize=2000, subchunksize=200)
 sme_indexes = sample(sme, 10)
 serialize(sme, "sme.jls")
-
-s100 = sample(sme, 100)
-serialize("s100.jls", s100)
-
-sme_probs = inclusion_prob(sme, 100)
-
-s1000 = sample(sme, 1000)
-serialize("s1000.jls", s1000)
-
-s10000 = sample(sme, 10000)
-serialize("s10000.jls", s10000)
-
-sme_probs = inclusion_prob(sme, 200)
