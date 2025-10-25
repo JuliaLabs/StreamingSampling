aef92c8e4b1d7474dca862e35f77a11a6cdb802c
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2025-10-24 22:13:16 -0400

    fix typo

--- examples/atomistic/cluster/srs-vs-sme-hfo2.sh
+++ examples/atomistic/cluster/srs-vs-sme-hfo2.sh
@@ -11,5 +11,5 @@ module load julia/1.10.4 # Engaging
 # Install Julia packages
 julia --project=./ -e 'import Pkg; Pkg.instantiate()'
 # Run the script
-julia --project=./ src-vs-sme-hfo2.jl
+julia --project=./ srs-vs-sme-hfo2.jl
 
